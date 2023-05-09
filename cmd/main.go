328b25419a857c75e48f9783c0a0d0494396bc13
topgunhh <1195958136@qq.com>
2023-05-10 00:57:53 +0800

    firest commit

---
+++ cmd/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// 初始化clientManager
+
+}
---
+++ pkg/client/manager.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type ClientManager struct {
+	kubeconfigPath string               // kubeconfig 连接哪个集群
+	insecureConfig *rest.Config         // 平台自用的rest.config
+	insecureClient kubernetes.Interface //平台自用的client-interface
+	k8sTokenKey    string               // 可配置的key的名字
+}
+
+func NewClientManager(kubeconfigPath, k8sTokenKey string) *ClientManager {
+	cm := &ClientManager{
+		kubeconfigPath: kubeconfigPath,
+		insecureConfig: nil,
+		insecureClient: nil,
+		k8sTokenKey:    k8sTokenKey,
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	cm.insecureConfig = config
+	cm.insecureClient = clientset
+
+	return cm
+}
